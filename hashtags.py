5b9088500cd252eefbc4cb27f9188d3829670ab6
suyashinpilani <h20180274@pilani.bits-pilani.ac.in>
2019-04-25 15:37:35 +0530

    copy of

---
+++ hashtags.py
@@ -0,0 +1,29 @@
+from config import *
+
+
+def insert_hashtags(hashtags, t):
+
+	for hashtag in hashtags:
+
+		query = "insert into hashtag_dim (hashtag_text) value ('{}')".format(hashtag)
+
+		try:
+			cursor.execute(query)
+			db.commit()
+			try:
+				cursor.execute("select hashtag_dim_id from hashtag_dim where hashtag_text='{}'".format(hashtag))
+
+				try:
+					h_d_id = cursor.fetchone()[0]
+
+					query = "insert into hashtag_tweet_bridge (hashtag_dim_id, tweet_dim_id) values ({}, {})".format(h_d_id, t)
+					cursor.execute(query)
+					db.commit()
+
+				except Exception as e:
+					pass
+			except Exception as e:
+				pass
+
+		except Exception as e:
+			pass
